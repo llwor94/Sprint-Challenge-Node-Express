58a57c08d13671e2f4cf7119886dbb2b28c2bf04
Lauren <llwor94@gmail.com>
2018-08-31 09:20:12 -0700

    action get methods set up

--- routes/actionsRoutes.js
+++ routes/actionsRoutes.js
@@ -3,4 +3,26 @@ const router = express.Router();
 
 const db = require('../data/helpers/actionModel');
 
+router.get('/', async (req, res, next) => {
+  try {
+    let data = await db.get();
+    res.status(200).json(data);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    let data = await db.get(req.params.id);
+    if (data) {
+      return res.status(200).json(data);
+    } else {
+      next({ statusCode: 404 });
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
 module.exports = router;
